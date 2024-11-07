befda8ec7a4afb348ca786e80f0fd60f523bb681
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-11-07 19:34:08 +0530

    v1.5 : Fixed Bugs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='optimization_techniques',
-    version='1.3',
+    version='1.5',
     packages=find_packages(),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
