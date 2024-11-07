6eb62688eede46699b3bb4311551cfbcb87cd17c
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-11-07 15:11:25 +0530

    Added In Pip

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Optimization Techniques',
-    version='0.1',
+    name='optimization_techniques',
+    version='1.3',
     packages=find_packages(),
-    install_requires=["numpy","random","matplotlib"],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Keerthivasan S',
     author_email='keerthi77459@gmail.com',
     description='Some Optimization Problems for Machine Learning',
-    url='https://github.com/KeerthiVasan-ai/optimization-techniques-lab', 
+    url='https://github.com/KeerthiVasan-ai/optimization-techniques', 
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License', 
